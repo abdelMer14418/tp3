df0b76be757d4f90c7598c879173f640a756aa27
Abdel <abdelhakmerabtene@gmail.com>
2024-03-31 16:18:36 +0200

    First commit

---
+++ db.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize')
+
+// database
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // User
+  process.env.DB_PASSWORD, // Password
+  {
+    host:  process.env.DB_HOST, // Host
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      createdAt: 'added',
+      updatedAt: 'updated',
+    }
+  },
+)
+
+sequelize.authenticate()
+sequelize.sync()
+
+module.exports = sequelize
